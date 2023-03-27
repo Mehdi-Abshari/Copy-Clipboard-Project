de1acb4597844ed9d6fadf8dca6f17afbec137f3
Mehdi-Abshari <absharimahdi5@gmail.com>
2023-03-27 07:17:53 +0430

    Add HTML code to JS and change all color

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,41 @@
-// button copy varibale
-const copyBtn = document.querySelector("button");
-// textarea varibale
-const textArea = document.querySelector("textarea");
+/* 
+  name: copy clipboard project JS file;
+  developer: Mehdi-Abshari;
+  production date: 1401-09-14;
+  description: ... .; 
+*/
+
+// Selecting body
+const body = document.querySelector('body'),
+// Create div for container
+ container = document.createElement('div'),
+// Create textArea for typing text
+ textAreaCr = document.createElement('textarea'),
+// Create button for copy text
+ button = document.createElement('button'),
+// button copy variable
+ copyBtn = document.querySelector("button"),
+// textarea variable
+ textArea = document.querySelector("textarea");
+
+ // Set class for container
+container.classList.add('container');
+// Set attribute for textArea
+textAreaCr.setAttribute('placeholder' , 'Type something ...');
+// Appending textAreaCr to container
+container.appendChild(textAreaCr);
+// Set inner text for button
+button.innerText = 'Copy it';
+// Appending button to container
+container.appendChild(button);
+// Appending container to body
+body.appendChild(container);
 
 // click event listener
 copyBtn.addEventListener("click", () => {
   // textarea selection
   textArea.select();
-  // textValue varibale
+  // textValue variable
   let textValue = textArea.value;
 
   // navigator clipboard copied
@@ -15,7 +43,7 @@ copyBtn.addEventListener("click", () => {
   // button copy inner
   copyBtn.innerText = "Copied !";
   // button copy background style
-  copyBtn.style.backgroundColor = "#03a9f4";
+  copyBtn.style.backgroundColor = "#1d7773";
 
   // time out set
   setTimeout(
@@ -23,7 +51,7 @@ copyBtn.addEventListener("click", () => {
       // after time out set button copy inner
       copyBtn.innerText = "Copy it";
       // after time out set button copy background style
-      copyBtn.style.backgroundColor = "#b3e5fc";
+      copyBtn.style.backgroundColor = "rgba(29, 119, 115, 0.8)";
     },
     // time in milliseconds
     1500
